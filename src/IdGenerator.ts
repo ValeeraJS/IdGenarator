d8b046deff70630938fabb954ff3f4bbf3a13ceb
大聪明 <601670314@qq.com>
2024-07-24 02:26:29 -0700

    Update IdGenerator.ts

--- src/IdGenerator.ts
+++ src/IdGenerator.ts
@@ -2,11 +2,11 @@ let value = 0;
 const decoder = new TextDecoder();
 const arrbuffer = new Uint8Array(32);
 
-export class IdGenerator {
-    static get serialNumber() {
-        return ++value;
-    }
-    static get randomString() {
-        return decoder.decode(crypto.getRandomValues(arrbuffer));
-    }
+export const IdGenerator = {
+	get serialNumber() {
+		return ++value;
+	},
+	get randomString() {
+		return decoder.decode(crypto.getRandomValues(arrbuffer));
+	}
 }

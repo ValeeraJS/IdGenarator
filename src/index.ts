af4398b542d8d1c44f5262a1ba92b1138724e1ee
大聪明 <601670314@qq.com>
2026-01-30 17:40:10 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,41 @@
-import { IdGenerator } from "./IdGenerator";
-export * from "./IIncreaser";
+const cache = new WeakMap<IdGenerator, number>();
 
-export default IdGenerator;
+/**
+ * @class
+ * @classdesc 数字id生成器，用于生成递增id
+ * @param {number} [initValue = 0] 从几开始生成递增id
+ * @implements IdGenerator.IIncreaser
+ */
+export class IdGenerator {
+	/**
+	 * @member IdGenerator.initValue
+	 * @desc id从该值开始递增，在创建实例时进行设置。设置之后将无法修改。
+	 * @readonly
+	 * @public
+	 */
+	public constructor(initValue = 0) {
+		cache.set(this, initValue);
+	}
+
+	/**
+	 * @method IdGenerator.prototype.next
+	 * @desc 生成新的id
+	 * @public
+	 * @returns {number} id
+	 */
+	public next(): number {
+		const v = cache.get(this)! + 1;
+		cache.set(this, v);
+		return v;
+	}
+
+	/**
+	 * @method IdGenerator.prototype.skip
+	 * @desc 生成新的32位uuid
+	 * @public
+	 * @returns {string} uuid
+	 */
+	public uuid(): string {
+		return crypto.randomUUID();
+	}
+}
